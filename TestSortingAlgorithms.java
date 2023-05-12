0e026bcef996af2268083447feb4a576851bf82e
Kailan <98kstewart@gmail.com>
2023-05-12 13:11:07 -0400

    new commit

--- TestSortingAlgorithms.java
+++ TestSortingAlgorithms.java
@@ -21,7 +21,60 @@ public class TestSortingAlgorithms {
       printArray(mergeArray); //trying to call print array, idk if this is correct though 
       
    public static Integer[] mergeSort(Integer[] array) {
-      //implement merge sort algorithm here. Want to call merge sort algorithm though. 
+      public void mergeSort(Object[] array) { //convert array argument into generic
+         if (array.length > 1) {
+            // split array into two halves
+            Object[] left = leftHalf(array); //changed int[] to Object[]
+            Object[] right = rightHalf(array); //yes, need here
+   
+            // recursively sort the two halves
+            mergeSort(left);
+            mergeSort(right);
+   
+            // merge the sorted halves into a sorted whole
+            merge(array, left, right);
+         }
+      }
+   
+       // Returns the first half of the given array.
+       public static Object[] leftHalf(Object[] array) { //yes, here (it's the variable left))
+           int size1 = array.length / 2; //don't need to convert to Integer or Object
+           Object[] left = new Object[size1]; //made changes here int --> Object 
+           for (int i = 0; i < size1; i++) { //don't need to convert int
+               left[i] = array[i];
+           }
+           return left; 
+       }
+   
+       // Returns the second half of the given array.
+       public static Object[] rightHalf(Object[] array) { //yes, here //EDITED
+           int size1 = array.length / 2;
+           int size2 = array.length - size1;
+           Object[] right = new Object[size2]; //edited
+           for (int i = 0; i < size2; i++) {
+               right[i] = array[i + size1];
+           }
+           return right;
+       }
+   
+       // Merges the given left and right arrays into the given
+       // result array.  Second, working version.
+       // pre : result is empty; left/right are sorted
+       // post: result contains result of merging sorted lists
+       public static void merge(Object[] result, Object[] left, Object[] right) { //yes, here (there are 2 variable declarations, i1, and i2) //edited
+           int i1 = 0;   // index into left array 
+           int i2 = 0;   // index into right array
+   
+           for (int i = 0; i < result.length; i++) {
+               if (i2 >= right.length || (i1 < left.length && (Integer)left[i1] <= (Integer)right[i2])) { //edited 
+                   result[i] = left[i1];    // take from left
+                   i1++;
+               } else {
+                   result[i] = right[i2];   // take from right
+                   i2++;
+               }
+           }
+       }
       return array; 
    }
       
